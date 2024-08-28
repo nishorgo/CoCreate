607335cce78ea76e57072d68a3618194c880dd13
Abid Hasan <abidhasan.bh@outlook.com>
2024-08-28 21:12:17 +0600

    feat: implemented live cursor chat and broadcasting in real time

--- components/Live.tsx
+++ components/Live.tsx
@@ -1,50 +1,92 @@
 import { useMyPresence, useOthers } from "@liveblocks/react";
-import React, { useCallback } from "react";
+import React, { useCallback, useEffect, useState } from "react";
 import LiveCursors from "./cursor/LiveCursors";
+import { CursorMode } from "@/types/type";
+import CursorChat from "./cursor/CursorChat";
 
 const Live = () => {
-    const others = useOthers();
-    const [{ cursor }, updateMyPresence] = useMyPresence() as any;
+  const others = useOthers();
+  const [{ cursor }, updateMyPresence] = useMyPresence() as any;
 
-    const handlePointerMove = useCallback((event: React.PointerEvent) => {
-        event.preventDefault();
+  const [cursorState, setCursorState] = useState({
+    mode: CursorMode.Hidden,
+  });
 
-        const x = event.clientX - event.currentTarget.getBoundingClientRect().x;
-        const y = event.clientY - event.currentTarget.getBoundingClientRect().y;
+  const handlePointerMove = useCallback((event: React.PointerEvent) => {
+    event.preventDefault();
 
-        updateMyPresence({ cursor: { x, y } });
-    }, []);
-    
-    const handlePointerLeave = useCallback((event: React.PointerEvent) => {
-        event.preventDefault();
+    const x = event.clientX - event.currentTarget.getBoundingClientRect().x;
+    const y = event.clientY - event.currentTarget.getBoundingClientRect().y;
 
-        const x = event.clientX - event.currentTarget.getBoundingClientRect().x;
-        const y = event.clientY - event.currentTarget.getBoundingClientRect().y;
+    updateMyPresence({ cursor: { x, y } });
+  }, []);
 
-        updateMyPresence({ cursor: null, message: null });
-    }, []);
+  const handlePointerLeave = useCallback((event: React.PointerEvent) => {
+    setCursorState({ mode: CursorMode.Hidden });
 
-    const handlePointerDown = useCallback((event: React.PointerEvent) => {
-      const x = event.clientX - event.currentTarget.getBoundingClientRect().x;
-      const y = event.clientY - event.currentTarget.getBoundingClientRect().y;
+    const x = event.clientX - event.currentTarget.getBoundingClientRect().x;
+    const y = event.clientY - event.currentTarget.getBoundingClientRect().y;
 
-      updateMyPresence({ cursor: { x, y } });
-    }, []);
+    updateMyPresence({ cursor: null, message: null });
+  }, []);
 
-    return (
-      <div
-        onPointerMove={handlePointerMove}
-        onPointerLeave={handlePointerLeave}
-        onPointerDown={handlePointerDown}
-        className="h-[100vh] w-full flex justify-center items-center text-center border-2 border-green-500"
-      >
-        <h1 className="text-5xl text-white">Liveblocks Co-Create</h1>
+  const handlePointerDown = useCallback((event: React.PointerEvent) => {
+    const x = event.clientX - event.currentTarget.getBoundingClientRect().x;
+    const y = event.clientY - event.currentTarget.getBoundingClientRect().y;
 
-        <LiveCursors others={others} />
-      </div>
-    );
-  
-    
-}
+    updateMyPresence({ cursor: { x, y } });
+  }, []);
 
-export default Live
+  useEffect(() => {
+    const onKeyUp = (e: KeyboardEvent) => {
+      if (e.key === "/") {
+        setCursorState({
+          mode: CursorMode.Chat,
+          previousMessage: null,
+          message: "",
+        });
+      } else if (e.key === "Escape") {
+        updateMyPresence({ message: "" });
+        setCursorState({ mode: CursorMode.Hidden });
+      }
+    };
+
+    const onKeyDown = (e: KeyboardEvent) => {
+      if (e.key === "/") {
+        e.preventDefault();
+      }
+    };
+
+    window.addEventListener("keyup", onKeyUp);
+    window.addEventListener("keydown", onKeyDown);
+
+    return () => {
+      window.removeEventListener("keyup", onKeyUp);
+      window.removeEventListener("keydown", onKeyDown);
+    };
+  }, [updateMyPresence]);
+
+  return (
+    <div
+      onPointerMove={handlePointerMove}
+      onPointerLeave={handlePointerLeave}
+      onPointerDown={handlePointerDown}
+      className="h-[100vh] w-full flex justify-center items-center text-center border-2 border-green-500"
+    >
+      <h1 className="text-5xl text-white">Liveblocks Co-Create</h1>
+
+      {cursor && (
+        <CursorChat
+          cursor={cursor}
+          cursorState={cursorState}
+          setCursorState={setCursorState}
+          updateMyPresence={updateMyPresence}
+        />
+      )}
+
+      <LiveCursors others={others} />
+    </div>
+  );
+};
+
+export default Live;
