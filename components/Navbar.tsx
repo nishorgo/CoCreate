12871d916caa7606fe20808eef82ece23651268e
Abid Hasan <abidhasan.bh@outlook.com>
2024-08-30 01:04:16 +0600

    refactor: set the logo

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -18,7 +18,7 @@ const Navbar = ({ activeElement, imageInputRef, handleImageUpload, handleActiveE
 
   return (
     <nav className="flex select-none items-center justify-between gap-4 bg-primary-black px-5 text-white">
-      <Image src="/assets/logo.svg" alt="FigPro Logo" width={58} height={20} />
+      <Image src="/assets/logo.svg" alt="CoCreate Logo" width={150} height={40} />
 
       <ul className="flex flex-row">
         {navElements.map((item: ActiveElement | any) => (
